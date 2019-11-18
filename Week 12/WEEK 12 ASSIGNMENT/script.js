5000580a76db015a4d0e0dd460a6c13cdfaba5d6
That one weirdo named Ana <hernandez_ana_merced@student.smc.edu>
2019-11-18 04:47:19 -0800

    Week 12 HW WIP
    Created a Javascript jquiery in order to try and hover each photo, then clickable to where their larger photo appears with a description. Struggling at the moment to hide the script toggles for the clickable part of the coding. Theme: SCP Foundation.

---
+++ Week 12/WEEK 12 ASSIGNMENT/script.js
@@ -0,0 +1,333 @@
+$(".gallery-01").hover(function () {
+            $('.caption').slideDown(400);
+        },
+        function () {
+            $('.caption').slideUp(400);
+        }
+);
+
+    // $('.gallery').hover(function(){
+    //     var hoverOver = $(this).getElementsByClassName();
+
+    //     if(hoverOver === '.gallery-01' ){
+    //         $('.caption').slideDown(1000)
+    //     }
+    // })
+
+
+    // Modal:  Click gallery image,  image full screen
+    var modal = document.getElementById("myModal");
+
+    var img = document.getElementById("gallery-image");
+    var modalImg = document.getElementById("image-01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Close modal
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+;
+
+$(".gallery-02").hover(function () {
+    $('.caption').slideDown(400);
+},
+function () {
+    $('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-02' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-02");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
+
+$(".gallery-03").hover(function () {
+    $('.caption').slideDown(400);
+},
+function () {
+    $('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-03' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-03");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
+
+$(".gallery-04").hover(function () {
+    $('.caption').slideDown(400);
+},
+function () {
+    $('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-04' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-04");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
+
+$(".gallery-05").hover(function () {
+$('.caption').slideDown(400);
+},
+function () {
+$('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-05' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-05");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
+
+$(".gallery-06").hover(function () {
+$('.caption').slideDown(400);
+},
+function () {
+$('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-06' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-06");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
+
+
+$(".gallery-07").hover(function () {
+    $('.caption').slideDown(400);
+},
+function () {
+    $('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-07' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-07");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
+
+$(".gallery-08").hover(function () {
+$('.caption').slideDown(400);
+},
+function () {
+$('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-08' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-08");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
+
+$(".gallery-09").hover(function () {
+$('.caption').slideDown(400);
+},
+function () {
+$('.caption').slideUp(400);
+}
+);
+
+// $('.gallery').hover(function(){
+//     var hoverOver = $(this).getElementsByClassName();
+
+//     if(hoverOver === '.gallery-09' ){
+//         $('.caption').slideDown(1000)
+//     }
+// })
+
+
+// Modal:  Click gallery image,  image full screen
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("image-09");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+modal.style.display = "block";
+modalImg.src = this.src;
+captionText.innerHTML = this.alt;
+}
+
+// Close modal
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+modal.style.display = "none";
+}
+;
